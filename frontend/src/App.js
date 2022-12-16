92642b3f815f2fabf11c3cf4e36add2d85de5cbf
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-12-17 04:38:36 +0530

    minor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import logo from'./under-construction.jpg';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Team Metatron</h1>
       <h2>This Site is under construction</h2>
+
+      <img src={logo}/>
     </div>
   );
 }
