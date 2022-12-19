7be87f35df527e9629c0de1beb4c370d3606f6a1
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-12-19 09:13:38 +0000

    added about and contct us page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <h1>Welcome to Team Metatron</h1>
       <h2>This Site is under construction</h2>
 
-      <img src={logo}/>
+      <img alt="under construction" src={logo}/>
     </div>
   );
 }
