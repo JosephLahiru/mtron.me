39fbd41b1c10b60dc012689520c978fbbcd046f2
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-12-29 22:34:53 +0530

    developing navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,19 @@ import {Routes, Route} from 'react-router-dom';
 import { About } from './components/about';
 import { Construction } from './components/construction';
 import Home from './components/home';
+import NavBar from './components/navbar';
 import { NoMatch } from './components/nomatch';
 
 function App(){
   return(
+    <>
       <Routes>
+      <NavBar/>
         <Route path='/' element={<Construction/>}></Route>
         <Route path='about' element={<About/>}></Route>
         <Route path='*' element={<NoMatch/>}></Route>
       </Routes>
+    </>
   );
 }
 
