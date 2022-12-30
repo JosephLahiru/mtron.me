4d021361ce134d06c386d999fb5493fa67662678
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-12-30 23:48:51 +0530

    minor

--- frontend/src/components/nomatch.js
+++ frontend/src/components/nomatch.js
@@ -1,6 +1,9 @@
 import './nomatch.css';
+import image from '.././notfound.jpg';
 
 export const NoMatch = () => {
     return <center><h1>404 Page Not Found</h1>
-    <p>Sorry, the page you are looking for could not be found.</p></center>
+    <p>Sorry, the page you are looking for could not be found.</p>
+    <img alt="not found" src={image}/>
+    </center>
 }
