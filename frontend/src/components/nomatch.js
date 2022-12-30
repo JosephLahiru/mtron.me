259ecb36a83dc31aac9377d064510bf9997f1856
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-12-30 23:39:26 +0530

    updated notfount page

--- frontend/src/components/nomatch.js
+++ frontend/src/components/nomatch.js
@@ -1,3 +1,6 @@
+import './nomatch.css';
+
 export const NoMatch = () => {
-    return <center><h1>404 Page Not Found</h1></center>
+    return <center><h1>404 Page Not Found</h1>
+    <p>Sorry, the page you are looking for could not be found.</p></center>
 }
