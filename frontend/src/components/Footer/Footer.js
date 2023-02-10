03a2a2f5bf5ffc8d684ae81ffcabc90353390e56
INFINITY <nirajnimantha@gmail.com>
2023-02-10 15:30:54 +0530

    init

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,45 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import { Link } from "react-router-dom";
+// reactstrap components
+import {
+  Button,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip
+} from "reactstrap";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col className="text-center" md="12">
+            <h1>Metatron</h1>
+            <p>Team Metatron is an AI startup that provides cutting-edge AI/ML services, as well as web, standalone, and mobile app development using Java, React, and Flutter.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
