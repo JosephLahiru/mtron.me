6189e0986df36a025defca9b6dcdeb30555f9837
theAlphaGuardian <joseph.rasanjana1@gmail.com>
2023-09-14 15:59:52 +0000

    minor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,27 @@
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import Index from "views/Index.js";
-import LandingPage from "views/examples/LandingPage.js";
-import RegisterPage from "views/examples/RegisterPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
+// import LandingPage from "views/examples/LandingPage.js";
+// import RegisterPage from "views/examples/RegisterPage.js";
+// import ProfilePage from "views/examples/ProfilePage.js";
 
-function App(){
-  return(
+function App() {
+  return (
     <BrowserRouter>
-    <Switch>
-      <Route path="/home" render={(props) => <Index {...props} />} />
-      <Route
-        path="/landing"
-        render={(props) => <LandingPage {...props} />}
-      />
-      <Route
+      <Switch>
+        <Route path="/home" render={(props) => <Index {...props} />} />
+        {/* <Route path="/landing" render={(props) => <LandingPage {...props} />} /> */}
+        {/* <Route
         path="/register"
         render={(props) => <RegisterPage {...props} />}
       />
       <Route
         path="/profile"
         render={(props) => <ProfilePage {...props} />}
-      />
-      <Redirect from="/" to="/home" />
-    </Switch>
-  </BrowserRouter>
+      /> */}
+        <Redirect from="/" to="/home" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
